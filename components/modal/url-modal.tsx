611311cc991a4660fb8f69258e1db9590df23ba1
irol765 <34485434+irol765@users.noreply.github.com>
2026-01-13 13:17:24 +0800

    Update url-modal.tsx

--- components/modal/url-modal.tsx
+++ components/modal/url-modal.tsx
@@ -29,7 +29,7 @@ const formSchema = z.object({
     .string()
     .toLowerCase()
     .min(2, { message: 'Please enter 2 or more characters.' })
-    .refine((string) => isSlug(string), {
+    .regex(/^[a-z0-9_-]+$/, {
       message: 'Please enter valid slug.'
     })
 });
